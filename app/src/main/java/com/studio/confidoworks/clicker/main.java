fb35cdcf2264e420bc76b991c5f6003ed4d30bc3
hellcatzulu <hellcatzulu@gmail.com>
2017-05-30 12:15:14 -0500

    1214 CSL: add creditsView & start setting up ADB

--- app/src/main/java/com/studio/confidoworks/clicker/main.java
+++ app/src/main/java/com/studio/confidoworks/clicker/main.java
@@ -2,15 +2,30 @@ package com.studio.confidoworks.clicker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class main extends Activity
 {
+    TextView creditsView  = (TextView)findViewById(R.id.creditsView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void viewCredits (View credits)
+    {
+        if (creditsView.getVisibility() == View.GONE)
+        {
+            creditsView.setVisibility(View.VISIBLE);
+        }
+        else if (creditsView.getVisibility() == View.VISIBLE)
+        {
+            creditsView.setVisibility(View.GONE);
+        }
     }
 
 }
