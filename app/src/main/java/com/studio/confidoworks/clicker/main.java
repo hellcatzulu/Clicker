322a6ae1dd95addef8a629536995ee1fb045c9d3
hellcatzulu <hellcatzulu@gmail.com>
2017-05-30 21:18:22 -0500

    2117 setup basic click mechanism

--- app/src/main/java/com/studio/confidoworks/clicker/main.java
+++ app/src/main/java/com/studio/confidoworks/clicker/main.java
@@ -2,11 +2,14 @@ package com.studio.confidoworks.clicker;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
 import android.widget.*;
+import com.studio.confidoworks.clicker.variables.*;
 
 public class main extends Activity
 {
+    TextView clickView = (TextView)findViewById(R.id.clickView);
+    TextView clickPSView = (TextView)findViewById(R.id.clickPSView);
     TextView creditsView  = (TextView)findViewById(R.id.creditsView);
 
     @Override
@@ -14,6 +17,22 @@ public class main extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        variables obj = new variables();
+        obj.load();
+        clickView.setText(variables.clicks + getString(R.string.clickView));
+        clickPSView.setText(variables.cps + getString(R.string.clickPSView));
+    }
+
+    public void click(View clicker)
+    {
+        variables.clicks = variables.clicks + variables.clicksPC;
+        clickView.setText(variables.clicks + getString(R.string.clickView));
     }
 
     public void viewCredits (View credits)
--- app/src/main/java/com/studio/confidoworks/clicker/variables.java
+++ app/src/main/java/com/studio/confidoworks/clicker/variables.java
@@ -1,43 +1,42 @@
 package com.studio.confidoworks.clicker;
 
-/**
- * created by hellcat-zulu on 24-May-17
- */
-
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class variables
+class variables
 {
-    public static final String PREFS_NAME = "var"; //primitive save file name
-    Context app;                                   //defined context for SharedPreferences
+    private static final String PREFS_NAME = "var";
+     Context app;
     //public String var;
     //public int var1
 
-    public int clicks;
-    public int cps;
-    public int store1Num;
-    public int store1Price;
-    public int store2Num;
-    public int store2Price;
+    static int clicks;
+    static int cps;
+    static int clicksPC;
+    static int store1Num;
+    static int store1Price;
+    static int store2Num;
+    static int store2Price;
 
-    public SharedPreferences varReader = app.getSharedPreferences(PREFS_NAME, 0);
-    public SharedPreferences.Editor varSaver = varReader.edit();
+    SharedPreferences varReader = app.getSharedPreferences(PREFS_NAME, 0);
+    SharedPreferences.Editor varSaver = varReader.edit();
 
-    public void load()
+    void load()
     {
         clicks = varReader.getInt("clicks", 0);
         cps = varReader.getInt("cps", 0);
+        clicksPC = varReader.getInt("clicksPC", 1);
         store1Num = varReader.getInt("store1Num", 0);
         store1Price = varReader.getInt("store1Price", 1);
         store2Num = varReader.getInt("store2Num", 0);
         store2Price = varReader.getInt("store2Price", 2);
     }
 
-    public void fullSave()
+    void fullSave()
     {
         varSaver.putInt("clicks", clicks);
         varSaver.putInt("cps", cps);
+        varSaver.putInt("clicksPC", clicksPC);
         varSaver.putInt("store1Num", store1Num);
         varSaver.putInt("store1Price", store1Price);
         varSaver.putInt("store2Num", store2Num);
